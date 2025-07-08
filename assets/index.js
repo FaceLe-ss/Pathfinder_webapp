5aa5424a087f42b1c5526bfee3c775d8fad97ee7
FaceLe-ss <vitalikzay2@gmail.com>
2025-07-08 10:53:56 +0300

    Add files via upload

--- assets/index.js
+++ assets/index.js
@@ -1,1 +1,12 @@
-// заглушка js файла
+function exportJSON() {
+  const character = {
+    name: document.getElementById('char-name').value,
+    class: document.getElementById('char-class').value,
+    level: document.getElementById('char-level').value
+  };
+  const blob = new Blob([JSON.stringify(character, null, 2)], { type: "application/json" });
+  const link = document.createElement("a");
+  link.href = URL.createObjectURL(blob);
+  link.download = "character.json";
+  link.click();
+}
